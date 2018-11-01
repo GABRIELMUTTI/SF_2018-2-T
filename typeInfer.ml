b01911a47f6e5c045dbe31a585b05c253a035dc1
David Mees Knijnik <kdavidk98@gmail.com>
2018-11-01 19:43:58 -0300

    Creation of "add" rule for the type collector

--- typeInfer.ml
+++ typeInfer.ml
@@ -5,20 +5,20 @@ type expType = TyBool
 			| TyNat  
 			|TyImplication of expType * expType
 			
-type typeVar = (int * expType)
 
-type typeEquation= TyAssign of typeVar * expType
+type typeEquation= TyAssign of expType * expType
 
 let rec collect (typeEnv : (expr * expType) list) (expr: Syntax.expr) = (*let typeEnv = (expr * extType) list*)
 	match expr with
     (* Rule SUM *)
-	Binop(Sum, e1, e2)->let (t1, c1) = collect typeEnv e1 in 
-        let (t2, c2) = collect typeEnv, e2 in 
-		let typeEnv= (Binop(Sum, e1, e2), TyNat)::typeEnv in (*return type of expression later*)
-        let C=c1 in
-		let C=c2@c1 in
-		let C=TyAssign(t1,TyNat)::C in
-        let C=TyAssign(t2,TyNat)::C in(*let C=[TyAssign(t1,TyNat);TyAssign(t2,TyNat)] in*)
-		(TyNat,C)
+	Binop(Sum, e1, e2)->let typeEnv= (Binop(Sum, e1, e2), TyNat)::typeEnv in
+		let (t1, c1) = collect typeEnv e1 in 
+        let (t2, c2) = collect typeEnv e2 in 
+		let typeEquations=[TyAssign(t1, TyNat);TyAssign(t2,TyNat)] in
+		let typeEquations= typeEquations@c1 in
+		let typeEquations= typeEquations@c2 in
+		(TyNat,typeEquations);;
+		
+		
                 
 
